18451ccdcbfa94bfcfc9ceaccc2b6bab5fe3ebcc
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-12-26 23:30:02 -0300

    transformed it into a Promisse

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+ //queremos que o terceiro apenas seja executado se o segundo for executado primeiro, por isso a Promisse
+
 function executaSegundo(){
-    setTimeout(function(){ //está se dividindo em Threads
-        console.log("SEGUNDO");
-    },1500);
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){ //está se dividindo em Threads
+            console.log("SEGUNDO");
+        },1500);
+    });
 }
 
 console.log("PRIMEIRO");
-executaSegundo();
+executaSegundo(); //chamdo a function
 console.log("TERCEIRO");
